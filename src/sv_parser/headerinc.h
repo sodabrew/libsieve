f5887413c615a291e29c81e99cf4e7aa4ca3931b
sodabrew <sodabrew@4a83aff7-be1d-0410-a803-db77f3e2a5ba>
2007-10-12 18:51:31 +0000

    Removed warnings due to const strings.

--- src/sv_parser/headerinc.h
+++ src/sv_parser/headerinc.h
@@ -9,7 +9,7 @@
 int libsieve_headerlex(void);
 int libsieve_headerparse(void);
 int libsieve_headerinput(char *buf, int max_size);
-void libsieve_headererror(const char *str);
+void libsieve_headererror(char *str);
 int libsieve_headerappend(header_list_t **hl);
 void libsieve_headerentry(header_t *h, char *name, char *body);
 
